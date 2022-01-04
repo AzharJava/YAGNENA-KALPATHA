d7b32d0ca29db8d4101c45465bf541077f11ada0
AzharJava <azharuddinsk7@gmail.com>
2022-01-04 19:53:55 +0530

    base_state change

--- lib/shared/base/base_widget.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-mixin BaseWidget {
-  getEmptyAppBar() =>
-      PreferredSize(child: Container(), preferredSize: Size(0, 0));
-  buildBottomSheet(context, widget) {
-    //showModalBottomSheet(context: context, builder: (context)=> Padding(p))
-  }
-}
